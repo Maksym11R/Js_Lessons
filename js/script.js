9984244d78fd126b74a551e9aa988da48280fb5b
Maksym <divine.anti.mage@gmail.com>
2022-09-30 12:40:11 +0300

    js practice

--- js/script.js
+++ js/script.js
@@ -10,12 +10,31 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один з останніх переглянутих фільмів', ''),
-	b = prompt('На скільки оціните його?', ''),
-	c = prompt('Один з останніх переглянутих фільмів', ''),
-	d = prompt('На скільки оціните його?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один з останніх переглянутих фільмів', ''),
+		b = prompt('На скільки оціните його?', '');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+
+}
+
+if (personalMovieDB.coun < 10) {
+	console.log("Перегоянуто замало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("Ви молодець");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Ви кіноман");
+} else {
+	console.log("error"); 1100
+}
 
 console.log(personalMovieDB);
+
